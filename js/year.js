d17ba22fb16b125c74a6d1cf80a694eef78cd2f8
AliciaPeraza <48078886+AliciaPeraza@users.noreply.github.com>
2022-02-17 11:13:36 -0800

    all original files

---
+++ js/year.js
@@ -0,0 +1,8 @@
+
+           
+    // function Year(){ 
+                
+        copyright=new Date();
+        update=copyright.getFullYear();
+        document.getElementById('thisYear').innerHTML = "Copyright © MAVI " + update;
+        // }
